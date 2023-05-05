ebd5cc6f6d85debe5f3752fbbf2b1814dbd8b4d7
marcorsouza <marco.rsouza@gmail.com>
2023-05-05 01:51:49 -0300

    metodo não utilizado
    metodo não utilizado

--- src/game/gameView.py
+++ src/game/gameView.py
@@ -8,12 +8,6 @@ class GameView(BaseClass):
         self.__players = players
         self.__dealer = dealer
     
-    def display_initial_cards(self):
-        self.logger.log("\nInitial cards:")
-        for player in self.__players:
-            self.logger.log(f"{player.get_user().name}: {[card.name + ' ' + card.suit.symbol for card in player.hand.get_cards()]}")
-        self.logger.log(f"Dealer: [{self.__dealer.get_player().hand.get_cards()[0].name} {self.__dealer.get_player().get_hand()[0].suit.symbol}, *]")
-
     def display_player_cards(self, player: Participant):
         self.logger.log(f"{player.get_user().name}'s cards: {[card.name + ' ' + card.suit.symbol for card in player.hand.get_cards()]} - Points: {player.hand.get_value()}")
 
