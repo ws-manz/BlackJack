fb9b6b1bb4ae2601a88a94a1f9ce0f57afac0d21
Marco Souza <86129076+ws-manz@users.noreply.github.com>
2023-05-08 14:04:54 -0300

    Added custom messages for player choices
    Updated the game logic to include custom messages for player choices. Random messages are now selected from predefined lists of hits, stands, and surrenders, providing a more dynamic and engaging experience for the player.

--- src/player/bot.py
+++ src/player/bot.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -10,5 +11,11 @@ class Bot(Participant):
     def __init__(self, user) -> None:
         super().__init__(user)
             
-    def wants_to_hit(self) -> bool:        
-        return self.hand.get_value() < self.get_user().level.value[1]        
+    def wants_to_hit(self) -> bool:   
+
+        is_hit = self.hand.get_value() < self.get_user().level.value[1]        
+        if(is_hit):
+            self.logger.log(f"{self.get_user().name} says: {random.choice(super().HITS)}") 
+        else:
+            self.logger.log(f"{self.get_user().name} says: {random.choice(super().STANDS)}") 
+        return is_hit
