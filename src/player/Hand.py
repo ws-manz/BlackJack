61e9f62d8cfe52aac109dbe26cc0d153129aeb97
marcorsouza <marco.rsouza@gmail.com>
2023-05-08 23:09:15 -0300

    Fixing "strip error" issue
    Previously, there was an issue in the code where the game would move to the second hand instead of splitting the hand when the player drew their second card. This issue has now been fixed.
    
    To avoid confusion, the code has also been updated to use English variable and function names. Additionally, a global variable named BLACKJACK has been added to the code to represent the value of 21 in Blackjack, and the name of this variable has been improved to better reflect its purpose.

--- src/player/Hand.py
+++ src/player/Hand.py
@@ -6,6 +6,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from card.card import Card
 
 class Hand:
+    
+    BLACKJACK = 21
+    
     def __init__(self, cards: list[Card] = None):
         
         if cards is None:
@@ -47,24 +50,24 @@ class Hand:
     def get_value(self) -> int:
         hand_value = 0
         aces_count = 0
-        for card in self.__cards:
+        for card in self:
             if card.name != 'Ace':
                 hand_value += card.value
             else:
                 aces_count += 1
 
         for i in range(aces_count):
-            if hand_value + 11 <= 21:
+            if hand_value + 11 <= self.BLACKJACK:
                 hand_value += 11
             else:
                 hand_value += 1
         return hand_value
 
     def is_bust(self) -> bool:
-        return self.get_value() > 21
+        return self.get_value() > self.BLACKJACK
 
     def is_blackjack(self) -> bool:
-        return self.get_value()== 21
+        return self.get_value() == self.BLACKJACK
     
     def __getitem__(self, index: int) -> Card:
         return self.__cards[index]
